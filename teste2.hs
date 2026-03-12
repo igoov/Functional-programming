2345ba82ef11562e58b95ae234ab6547e0dcc00e
igor <igorbernardorocha@gmail.com>
2026-03-12 01:46:57 -0300

    Removendo arquivos que apaguei

--- teste2.hs
+++
@@ -1,7 +0,0 @@
-main = do
-    putStrLn "Digite o primeiro numero:"
-    a <- getLine
-    putStrLn "Digite o segundo numero:"
-    b <- getLine
-    let soma = (read a :: Int) + (read b :: Int)
-    putStrLn ("A soma é: " ++ show soma)
