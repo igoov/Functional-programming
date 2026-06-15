ea5a8ef3f1026388ef1e86ee3c895d2a549b9a51
igor <igorbernardorocha@gmail.com>
2026-06-15 20:52:57 -0300

    Multiplicação em lista

---  Functional-programming/Lista/Lista Avaliativa 2/q3.hs
+++  Functional-programming/Lista/Lista Avaliativa 2/q3.hs
@@ -1,12 +1,26 @@
-fibAux :: Int -> Int -> Int -> Int
-fibAux 0 a _ = a
-fibAux n a b = fibAux (n - 1) b (a + b)
+multiplica :: Int -> [(Int,Int,Int)]
 
-fibonacci :: Int -> Int
-fibonacci n = fibAux n 0 1
+multiplica n = aux n 1
+
+
+
+aux :: Int -> Int -> [(Int,Int,Int)]
+
+aux n contador
+
+    | contador > 10 = []
+
+    | otherwise =
+        (n,contador,n*contador) :
+        aux n (contador+1)
+
+
 
 main :: IO ()
+
 main = do
-    x <- getLine
-    let n = read x :: Int
-    print (fibonacci n)
+
+    putStrLn "Digite um numero:"
+    n <- readLn
+
+    print (multiplica n)
