8975f251357f8dfee110651220a13845be990072
igor <igorbernardorocha@gmail.com>
2026-06-15 20:54:40 -0300

    Maior string

---  Functional-programming/Lista/Lista Avaliativa 2/q6.hs
+++  Functional-programming/Lista/Lista Avaliativa 2/q6.hs
@@ -1,12 +1,34 @@
-imprimir :: Int -> IO ()
-imprimir n
-    | n <= 0 = return ()
-    | otherwise = do
-        imprimir (n - 1)
-        print n
+tamanho :: String -> Int
+
+tamanho [] = 0
+
+
+tamanho (x:xs) =
+    1 + tamanho xs
+
+
+
+maiorString :: String -> String -> String
+
+
+maiorString a b
+
+    | tamanho a > tamanho b = a
+
+    | otherwise = b
+
+
 
 main :: IO ()
+
 main = do
-    x <- getLine
-    let n = read x :: Int
-    imprimir n
+
+    putStrLn "Primeira string:"
+    a <- getLine
+
+
+    putStrLn "Segunda string:"
+    b <- getLine
+
+
+    putStrLn (maiorString a b)
